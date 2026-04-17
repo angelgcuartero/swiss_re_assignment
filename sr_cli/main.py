c186422d15f4ea14d6c684bdaf2368c49b914652
Ángel García <anhelido@gmail.com>
2026-04-17 11:06:34 +0200

    feat: Add processing functions and doctrings

--- sr_cli/main.py
+++ sr_cli/main.py
@@ -8,11 +8,13 @@ from typing import Annotated, List
 import typer
 
 from sr_cli.output import generate_output_file
-from sr_cli.process import process_log_files
+from sr_cli.process import process_files
 
 # Set up logging configuration
 log_level = os.getenv("LOG_LEVEL", "INFO").upper()
-logging.basicConfig(level=log_level, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logging.basicConfig(
+    level=log_level, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
 log = logging.getLogger(__name__)
 app = typer.Typer()
 
@@ -31,10 +33,18 @@ def process(
         ),
     ],
     output: Annotated[Path, typer.Argument(help="Path to the output file")],
-    mfip: Annotated[bool, typer.Option("--mfip", help="Calculate the most frequent IP")] = False,
-    lfip: Annotated[bool, typer.Option("--lfip", help="Calculate the least frequent IP")] = False,
-    eps: Annotated[bool, typer.Option("--eps", help="Calculate events per second")] = False,
-    bytes: Annotated[bool, typer.Option("--bytes", help="Calculate total bytes exchanged")] = False,
+    mfip: Annotated[
+        bool, typer.Option("--mfip", help="Calculate the most frequent IP")
+    ] = False,
+    lfip: Annotated[
+        bool, typer.Option("--lfip", help="Calculate the least frequent IP")
+    ] = False,
+    eps: Annotated[
+        bool, typer.Option("--eps", help="Calculate events per second")
+    ] = False,
+    bytes: Annotated[
+        bool, typer.Option("--bytes", help="Calculate total bytes exchanged")
+    ] = False,
 ):
     """Do main CLI task for the swiss-re-assignment.
 
@@ -47,6 +57,7 @@ def process(
         lfip (bool): Flag to calculate the least frequent IP.
         eps (bool): Flag to calculate events per second.
         bytes (bool): Flag to calculate total bytes exchanged.
+
     """
     # Pass the flags as keyword arguments to the processing function
     kwargs = {
@@ -55,8 +66,8 @@ def process(
         "calculate_eps": eps,
         "calculate_bytes": bytes,
     }
-    res_num_lines, res_mfip, res_lfip, res_eps, res_bytes = process_log_files(input, output, **kwargs)
-    generate_output_file(output, res_num_lines, res_mfip, res_lfip, res_eps, res_bytes)
+    process_response = process_files(input, output, **kwargs)
+    generate_output_file(output, process_response)
 
 
 if __name__ == "__main__":
--- sr_cli/process.py
+++ sr_cli/process.py
@@ -8,83 +8,116 @@ from collections import Counter
 
 # Set up logging configuration
 log_level = os.getenv("LOG_LEVEL", "INFO").upper()
-logging.basicConfig(level=log_level, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logging.basicConfig(
+    level=log_level, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
 log = logging.getLogger(__name__)
 
 
-# Sample log line format:
-# Field 1: 1157689324.156 [Timestamp in seconds since the epoch]
-# Field 2: 1372 [Response header size in bytes]
-# Field 3: 10.105.21.199 [Client IP address]
-# Field 4: TCP_MISS/200 [HTTP response code]
-# Field 5: 399 [Response size in bytes]
-# Field 6: GET [HTTP request method]
-# Field 7: http://www.google-analytics.com/__utm.gif? [URL]
-# Field 8: badeyek [Username]
-# Field 9: DIRECT/66.102.9.147 [Type of access/destination IP address]
-# Field 10: image/gif [Response type]
-
-
-def process_log_files(
+def process_files(
     input_file: List[str] | str,
     output_file: str,
     **kwargs,
-) -> tuple:
-    """Process log files and perform calculations based on the provided flags."""
+) -> list[dict[str, int | str | float]]:
+    """Process log files and perform calculations based on the provided flags.
+
+    Args:
+        input_file (List[str] | str): A list of input file paths or a single input file path.
+        output_file (str): The output file path where results will be saved.
+        **kwargs: Additional keyword arguments for processing.
+
+    Returns:
+        list[dict[str, int | str | float]]: A list of dictionaries containing the results of processing each input file.
+
+    """
     input_files = [input_file] if isinstance(input_file, str) else input_file
+    response = []
 
-    # for file in input_files:
-    #     log.debug(f"Input file: {file}")
-    # log.debug(f"Output file: {output_file}")
-    # log.debug(f"Most frequent IP flag: {kwargs.get('calculate_mfip', False)}")
-    # log.debug(f"Least frequent IP flag: {kwargs.get('calculate_lfip', False)}")
-    # log.debug(f"Events per second flag: {kwargs.get('calculate_eps', False)}")
-    # log.debug(f"Total bytes exchanged flag: {kwargs.get('calculate_bytes', False)}")
+    # Log the input and output file paths and any additional keyword arguments
+    for file in input_files:
+        log.debug(f"Input file: {file}")
+    log.debug(f"Output file: {output_file}")
+    for key, value in kwargs.items():
+        log.debug(f"{key}: {value}")
 
-    # Measure the time taken to read the file
-    start_time = time.perf_counter()
-    num_lines, mfip, lfip, eps, total_bytes = read_data_file(input_files[0])
-    end_time = time.perf_counter()
-    total_time = end_time - start_time
-
-    log.info(f"Time taken to read the file: {total_time:.4f} seconds")
-    log.info(f"Total lines read: {num_lines}")
-    log.info(f"Most frequent IP: {mfip}")
-    log.info(f"Least frequent IP: {lfip}")
-    log.info(f"Events per second: {eps}")
-    log.info(f"Total bytes exchanged: {total_bytes}")
-    return num_lines, mfip, lfip, eps, total_bytes
+    for file in input_files:
+        log.debug(f"Processing file: {file}")
+        result_dict = read_data_file(file)
+        for key, value in result_dict.items():
+            log.debug(f"{key}: {value}")
+        response.append(result_dict)
+    return response
 
 
-def read_data_file(file_path: str) -> tuple[int, str, str, float, int]:
-    """Read the log file and process its contents."""
+def read_data_file(file_path: str) -> dict[str, int | str | float]:
+    """Read the log file and process its contents.
+
+    Args:
+        file_path (str): The path to the log file to be processed.
+
+    Returns:
+        dict[str, int | str | float]: A dictionary containing the processed results.
+
+    """
     res_num_lines = 0
     res_mfip = ""
     res_lfip = ""
     res_eps: float = 0.0
     res_bytes: int = 0
+    res_ip = Counter()
+
+    start_time = time.perf_counter()
     with open(file_path, mode="r") as file:
         for line in file:
-            # fields = line.strip().split(" ", 1)
             if not line.strip():
                 continue
-            # if len(fields) < 2:
-            #     continue
             res_num_lines += 1
             response_header_size, client_ip, response_size = parse_line(line.strip())
             res_bytes += response_header_size + response_size
+            res_ip[client_ip] += 1
+    end_time = time.perf_counter()
+    total_time = end_time - start_time
 
-    return res_num_lines, res_mfip, res_lfip, res_eps, res_bytes
+    # Calculate most and least frequent IPs
+    if res_ip:
+        res_mfip = res_ip.most_common(1)[0][0]
+        res_lfip = res_ip.most_common()[-1][0]
+
+    # Calculate events per second
+    res_eps = (
+        res_num_lines / total_time if total_time > 0 else None
+    )  # Avoid division by zero
+
+    return {
+        "num_lines": res_num_lines,
+        "mfip": res_mfip,
+        "lfip": res_lfip,
+        "eps": res_eps,
+        "bytes": res_bytes,
+    }
 
 
 def parse_line(fields: List[str]) -> tuple[int, str, int]:
-    """Parse a log line and extract relevant fields."""
+    """Parse a log line and extract relevant fields.
+
+    Args:
+        fields (List[str]): A list of strings representing the fields in a log line.
+
+    Returns:
+        tuple[int, str, int]: A tuple containing the response header size, client IP, and response size.
+
+    """
+    # Discard the first field (timestamp) and split the rest of the fields
     _, rest_of_fields = fields.strip().split(" ", 1)
     rest_of_fields = rest_of_fields.lstrip().split(" ")
-    response_header_size_str = rest_of_fields[0]
-    client_ip = rest_of_fields[1]
-    response_size_str = rest_of_fields[3]
+    response_header_size_str = rest_of_fields[
+        0
+    ]  # Field 2: 1372 [Response header size in bytes]
+    client_ip = rest_of_fields[1]  # Field 3: 10.105.21.199 [Client IP address]
+    response_size_str = rest_of_fields[3]  # Field 5: 399 [Response size in bytes]
 
-    response_header_size = int(response_header_size_str) if response_header_size_str.isdigit() else 0
+    response_header_size = (
+        int(response_header_size_str) if response_header_size_str.isdigit() else 0
+    )
     response_size = int(response_size_str) if response_size_str.isdigit() else 0
     return response_header_size, client_ip, response_size
